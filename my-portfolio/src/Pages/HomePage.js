c938eb2e0e650a2d2fb04ec6e14774c0bdcdc356
Yakup <yakup.atahanow.b@gmail.com>
2025-07-04 15:42:18 -0700

    Prettied the HomePage

--- my-portfolio/src/Pages/HomePage.js
+++ my-portfolio/src/Pages/HomePage.js
@@ -1,15 +1,19 @@
+import profileImg from '../Assets/ProfilePic.jpg';
 import React from 'react';
 import './HomePage.css';
-import profileImg from '../Assets/ProfilePic.jpg';
 
 export default function HomePage() {
   return (
     <div className="homepage-container">
       <div className="dot-background" />
-      
-      <div className="hero-content">
-        <img src={profileImg} alt="Yakup Atahanov" className="profile-image" />
-        <h1 className="hero-text">I am Yakup Atahanov</h1>
+
+      <div className="hero-section">
+        <div className="hero-text">
+          <h1>I am Yakup Atahanov</h1>
+        </div>
+        <div className="hero-image">
+          <img src={profileImg} alt="Yakup Atahanov" />
+        </div>
       </div>
 
       <div className="page-indicator">
